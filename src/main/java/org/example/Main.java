947262ccb5c4307d7f8ad2126146a3faf84aef71
Apollo <sv.apollo11@gmail.com>
2025-09-27 08:15:26 -0300

    :sparkles: feat: adc ponto de entrada com inicialização do CalculatorView

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.example.view.CalculatorView;
+
+public class Main {
+    public static void main(String[] args) {
+        CalculatorView view = new CalculatorView();
+        view.start();
+    }
+}
