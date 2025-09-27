01e649e66611c5219bb7cd03d98fac66af562a57
Apollo <sv.apollo11@gmail.com>
2025-09-27 08:18:33 -0300

    :sparkles: feat: logica de negocio para soma e subtração na calculadora

---
+++ src/main/java/org/example/model/Calculator.java
@@ -0,0 +1,14 @@
+package org.example.model;
+
+public class Calculator {
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    // TODO: Implementar multiplicação e divisão (tratar erro de divisão por zero)
+}
