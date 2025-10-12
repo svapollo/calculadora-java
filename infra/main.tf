085e8ae190a2554f0ec446ef602da85c814043af
Apollo <sv.apollo11@gmail.com>
2025-10-11 22:09:34 -0300

    :bricks:	ci: Subir um .jar ao invés do .zip

--- infra/main.tf
+++ infra/main.tf
@@ -6,10 +6,10 @@ resource "aws_s3_bucket" "lambda_bucket" {
   bucket = "calculadora-java-lambda-bucket"
 }
 
-resource "aws_s3_object" "lambda_zip" {
+resource "aws_s3_object" "lambda_jar" {
   bucket = aws_s3_bucket.lambda_bucket.bucket
-  key    = "calculadora-java.zip"
-  source = "../target/calculadora-java.zip"
+  key    = "calculadora-java-1.0-SNAPSHOT.jar"
+  source = "../target/calculadora-java-1.0-SNAPSHOT.jar"
 }
 
 resource "aws_iam_role" "lambda_exec" {
@@ -37,8 +37,8 @@ resource "aws_lambda_function" "calculadora_lambda" {
   handler       = "org.example.Main::handleRequest"
   role          = aws_iam_role.lambda_exec.arn
   s3_bucket     = aws_s3_bucket.lambda_bucket.id
-  s3_key        = aws_s3_object.lambda_zip.key
+  s3_key        = aws_s3_object.lambda_jar.key
 
-  depends_on = [aws_s3_object.lambda_zip]
+  depends_on = [aws_s3_object.lambda_jar]
 }
 
